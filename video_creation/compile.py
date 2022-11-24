cfd5a96b0c2c745dc7b6b5ceadac3c4d26cc1d18
yeeterlol <81702700+yeeterlol@users.noreply.github.com>
2022-11-23 19:48:54 -0800

    Update compile.py

--- video_creation/compile.py
+++ video_creation/compile.py
@@ -4,12 +4,10 @@ from utils import cleanup
 import random
 
 def compile_video():
-    ran = random.choice(os.listdir("./assets/temp/png/"))
-    generate = random.randint(1, 999999)
     # Clip
     clip = VideoFileClip(f"./assets/temp/background.mp4").subclip(0, 15).without_audio().resize(height=1920).crop(
         x1=1166.6, y1=0, x2=2246.6, y2=1920)
-    image = ImageClip(f"./assets/temp/png/{ran}").set_pos("center").resize(height=800).set_duration(15)
+    image = VideoFileClip(f"./assets/temp/background/family.mp4").set_pos("center", "top").resize(height=800).set_duration(15)
 
     combined = CompositeVideoClip([clip, image])
     Path("./results").mkdir(parents=True, exist_ok=True)
